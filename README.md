22392ab02210fcaa96f5f5bace0eabe520b03e40
Mitsutaka Takeda <mitsutaka.takeda@gmail.com>
2019-02-09 18:55:29 +0900

    use MonadError to abstract error handling

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ A sample project for how to implement tagless final with Cats Effect.
 - [Gabriel Volpe - Cats Effect: The IO Monad for Scala](https://www.youtube.com/watch?v=8_TWM2t97r4)
 - [Gabriel Volpe - Cats Effect: The IO Monad for Scala - Slides and codes](https://github.com/paidy/talks/tree/master/2018-10-30/scalaio-demo)
 - [Tagless Final StyleなRepositoryのHigher Kindが異なる場合のUse Caseの書き方](https://qiita.com/yoshiyoshifujii/items/eddc174a19d0a4cc51ce)
+- [Monad Error for the rest of us](https://www.codacy.com/blog/monad-error-for-the-rest-of-us/)
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,7 @@
 import cats._
 import cats.effect._
-import cats.syntax.functor._
 import cats.syntax.flatMap._
+import cats.syntax.functor._
 import io.getquill._
 
 import scala.concurrent.Future
@@ -17,7 +17,10 @@ object Main extends IOApp {
   case class User(id: UserId, name: String)
 
   object User {
-    def create(name: String): User = User(UserId(Random.alphanumeric.take(10).mkString), name)
+    def create(name: String): User = User(UserId(
+//      Random.alphanumeric.take(10).mkString
+      "me"
+    ), name)
   }
 
   trait UserRepository[F[_]] {
@@ -46,11 +49,19 @@ object Main extends IOApp {
     def readStr: F[String]
   }
 
-  class StdConsole[F[_]: Sync] extends Console[F] {
+  class StdConsole[F[_] : Sync] extends Console[F] {
     override def putStr(message: String): F[Unit] = Sync[F].delay(print(message))
 
     override def readStr: F[String] = Sync[F].delay(scala.io.StdIn.readLine())
   }
+
+  sealed trait UseCaseError extends Throwable
+
+  final case class UseCaseSystemError(detail: Throwable) extends UseCaseError
+
+  final case class UserIdAlreadyTaken() extends UseCaseError
+
+  type Throwing[F[_]] = ApplicativeError[F, Throwable]
 
   object AccountCreateUseCase {
     def execute[F[_] : Monad](name: String, email: String)
@@ -60,12 +71,17 @@ object Main extends IOApp {
     } yield s"$userId-$accountId"
   }
 
-  class PostgresUserRepository[F[_] : Async] extends UserRepository[F] {
+
+  class PostgresUserRepository[F[_] : Async]()(
+    implicit ME: ApplicativeError[F, Throwable]
+    // implicit ME: ApplicativeError[F, UseCaseError]　だとflatMapがみつからない。。。
+  ) extends UserRepository[F] {
     // Need a Escape strategy to use the table name of 'user' in postgres.
     // https://getquill.io/#contexts-sql-contexts-naming-strategy
     lazy val ctx = new PostgresAsyncContext(NamingStrategy(SnakeCase, Escape), "ctx")
 
     import ctx._
+
     import scala.concurrent.ExecutionContext.Implicits._
 
     private def fromFuture[A](fa: F[Future[A]]): F[A] =
@@ -78,12 +94,17 @@ object Main extends IOApp {
         }
       }
 
-    override def store(user: User): F[UserId] = {
-      val a = quote {
-        query[User].insert(lift(user))
-      }
-      fromFuture(Sync[F].delay(ctx.run(a).map(_ => user.id)))
-    }
+    override def store(user: User): F[UserId] =
+      for {
+        userId <- if (user.id == UserId("me")) {
+          ME.raiseError[UserId](UserIdAlreadyTaken())
+        } else {
+          val a = quote {
+            query[User].insert(lift(user))
+          }
+          fromFuture[UserId](Sync[F].delay(ctx.run(a).map(_ => user.id)))
+        }
+      } yield userId
 
     override def find(id: UserId): F[Option[User]] = {
       val a = quote {
@@ -92,6 +113,28 @@ object Main extends IOApp {
       fromFuture(Sync[F].delay(ctx.run(a).map(_.headOption)))
     }
   }
+
+
+  implicit def monadErrorInstanceForIo: MonadError[IO, UseCaseError] = new MonadError[IO, UseCaseError] {
+    override def raiseError[A](e: UseCaseError): IO[A] = IO.raiseError(e)
+
+    override def handleErrorWith[A](fa: IO[A])(f: UseCaseError => IO[A]): IO[A] = fa.handleErrorWith {
+      case u: UseCaseError => f(u)
+      case t => f(UseCaseSystemError(t))
+    }
+
+    override def pure[A](x: A): IO[A] = IO.pure(x)
+
+    override def ap[A, B](ff: IO[A => B])(fa: IO[A]): IO[B] = ff.flatMap(f => fa.map(f))
+
+    override def flatMap[A, B](fa: IO[A])(f: A => IO[B]): IO[B] = fa.flatMap(f)
+
+    override def tailRecM[A, B](a: A)(f: A => IO[Either[A, B]]): IO[B] = f(a).flatMap(r => r match {
+      case Left(_) => tailRecM(a)(f)
+      case Right(value) => IO.pure(value)
+    })
+  }
+
 
   class MemoryAccountRepository[F[_] : Sync](accounts: scala.collection.mutable.Map[AccountId, Account]) extends AccountRepository[F] {
     override def store(Account: Account): F[AccountId] = for {
@@ -103,17 +146,30 @@ object Main extends IOApp {
     } yield a
   }
 
-  class Controller[F[_] : Monad]
+  class Controller[F[_]]
   (
     U: UserRepository[F],
     A: AccountRepository[F],
     C: Console[F]
+  )
+  (
+    implicit val E: MonadError[F, Throwable]
   ) {
     def endpoint: F[Unit] = for {
-      result <- AccountCreateUseCase.execute("a user", "test@test")(Monad[F], U, A)
-      _ <- C.putStr(result)
+      result <-
+        E.handleError(
+          AccountCreateUseCase
+            .execute("a user", "test@test")(E, U, A)
+        ) {
+          case UseCaseSystemError(detail) =>
+            detail.toString
+          case UserIdAlreadyTaken() =>
+            "UserID is already taken"
+        }
+            _ <- C.putStr(result)
     } yield ()
   }
+
 
   // Inject implementations
   lazy val program: IO[Unit] = new Controller[IO](
